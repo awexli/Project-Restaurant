c93625ad1aa4e16d78d0bea8f448a6b795d9a140
Sunny Wong <swong6@mail.sfsu.edu>
2019-09-21 15:42:20 -0700

    Added reserve route

--- react-backend/app.js
+++ react-backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var reserveRouter = require('./routes/reserve');
 
 var app = express();
 //port
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/reserve', reserveRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
